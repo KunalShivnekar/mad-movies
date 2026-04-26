1da8e9e38bc46d7fe7230ea4fddca6c5c5e69821
KunalShivnekar <Kunal.Shivnekar@gmail.com>
2026-04-26 23:22:24 +0530

    Replaced LiveData with StateFlow

--- ui/moviesui/src/main/java/com/kshivnekar/moviesui/MoviesFragment.kt
+++ ui/moviesui/src/main/java/com/kshivnekar/moviesui/MoviesFragment.kt
@@ -1,28 +1,19 @@
 package com.kshivnekar.moviesui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.kshivnekar.arch.data.Response
-import com.kshivnekar.arch.data.succeeded
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kshivnekar.movies.R
-import com.kshivnekar.movies.databinding.FragmentMoviesBinding
-import com.kshivnekar.moviesdata.remote.ApiFilm
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 /**
  * A simple [Fragment] subclass.
@@ -36,7 +27,7 @@ class MoviesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                val movies = moviesViewModel.moviesState.observeAsState(Response.Loading).value
+                val movies by moviesViewModel.moviesState.collectAsStateWithLifecycle()
                 MoviesScreen(
                     movies = movies,
                     onMovieSelected = {},
--- ui/moviesui/src/main/java/com/kshivnekar/moviesui/MoviesViewModel.kt
+++ ui/moviesui/src/main/java/com/kshivnekar/moviesui/MoviesViewModel.kt
@@ -1,22 +1,23 @@
 package com.kshivnekar.moviesui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kshivnekar.arch.data.Response
 import com.kshivnekar.moviesdata.remote.ApiFilm
 import com.kshivnekar.moviesdata.repository.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MoviesViewModel @Inject constructor(private val movieRepository: MovieRepository):ViewModel() {
 
-    private val _moviesState = MutableLiveData<Response<List<ApiFilm>>>()
+    private val _moviesState = MutableStateFlow<Response<List<ApiFilm>>>(Response.Loading)
 
-    val moviesState: LiveData<Response<List<ApiFilm>>> = _moviesState
+    val moviesState: StateFlow<Response<List<ApiFilm>>> = _moviesState.asStateFlow()
 
     init {
         viewModelScope.launch {
